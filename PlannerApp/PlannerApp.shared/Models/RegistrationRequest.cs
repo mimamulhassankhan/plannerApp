ed6d927672c0654d4f2d0897299f8ec497c1c5a6
mimamulhassankhan <pappuhassan440@gmail.com>
2020-08-04 01:42:10 +0600

    first commit

---
+++ PlannerApp/PlannerApp.shared/Models/RegistrationRequest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace PlannerApp.shared.Models
+{
+	public class RegistrationRequest
+	{
+		[EmailAddress]
+		[Required]
+		[StringLength(50)]
+		public string EmailAddress { get; set; }
+
+		[Required]
+		[StringLength(25)]
+		public string FirstName { get; set; }
+
+		[Required]
+		[StringLength(25)]
+		public string LastName { get; set; }
+
+		[Required]
+		[StringLength(50)]
+		public string Password { get; set; }
+
+		[Required]
+		[StringLength(50)]
+		public string ConfirmPassword { get; set; }
+	}
+}
---
+++ PlannerApp/PlannerApp.shared/Models/UserManagerResponse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlannerApp.shared.Models
+{
+	public class UserManagerResponse
+	{
+		public string Message { get; set; }
+
+		public bool IsSuccess { get; set; }
+
+		public string[] Errors { get; set; }
+
+		public DateTime? ExpireDate { get; set; }
+	}
+}
